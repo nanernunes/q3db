e53eb48075d9c3321c9111983f9c7f3d63d032d7
Naner Nunes <naner@naner.com.br>
2014-08-04 03:46:42 -0300

    Added Gemfile to solution and RSpec Suite

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'quakeentry'
+require 'quaketrace'
+require 'match'
+require 'client'
+
+describe Client do
+	
+	before :each do
+		Match.create
+		QuakeEntry.new '0:02 ClientConnect: 1'
+	end
+
+	after :each do
+		Match.destroy_all
+		Client.destroy_all
+	end
+
+	it "should create a client connection on the last match" do
+		expect( Client.count ).to be_eql 1 
+	end
+
+	it "should change the nickname of the previous connected client" do
+		QuakeEntry.new '0:09 ClientUserinfoChanged: 1 n\Bones\t\0\model\bones/bones\tl\0'
+		expect( Client.last.nickname ).to be_eql "Bones"
+		expect( Client.last.model ).to be_eql "bones/bones"
+	end
+
+end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,13 +1,34 @@
-require_relative '../spec_helper'
+require 'spec_helper'
+require 'quakeentry'
+require 'match'
 
 describe Match do
 	
-	before :all do
-		@match = Match.create
-	end
-
-	after :all do
+	after :each do
 		Match.destroy_all
 	end
 
+	it "should create an empty match" do
+		Match.create
+		expect( Match.count ).to be_eql 1
+	end
+
+	it "should parse a match by regex" do
+		QuakeEntry.new (
+			'0:00 InitGame: \sv_hostname\QUAKE EXTREME BRASIL
+			\sv_minRate\0\sv_maxRate\10000\sv_dlRate\100\sv_minPing\0
+			\sv_maxPing\0\sv_floodProtect\1\dmflags\0\fraglimit\20\timelimit\0
+			\sv_maxclients\8\g_maxGameClients\0\capturelimit\8
+			\version\ioq3 1.36 linux-arm Aug  2 2012\com_protocol\71\g_gametype\0
+			\mapname\q3dm4\sv_privateClients\0\sv_allowDownload\0\bot_minplayers\0
+			\gamename\baseq3\g_needpass\0\Administrator\Naner Nunes\Email
+			\naner@naner.com.br\URL\http://q3.naner.us\Location\Brasil
+			\CPU\Raspberry-PI $35.00\mappack\http://www.naner.us/quake3.zip'
+		)
+		
+		@match = Match.last
+
+		expect( @match.sv_hostname ).to be_eql "QUAKE EXTREME BRASIL"
+
+	end
 end
