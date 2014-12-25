77041c7bd4856012e5f7532bab50063f97e66072
Naner Nunes <naner@naner.com.br>
2014-12-25 16:01:50 -0200

    Refactoring QuakeTrace Class to remove newline

--- lib/quaketrace.rb
+++ lib/quaketrace.rb
@@ -4,14 +4,16 @@ require 'quakecolor'
 
 module QuakeTrace
 
-  def self.notice(counter)
-    print "\n" if counter.joins.zero?
-    print ERB.new( File.read(File.join(
-      'app', 'views', 'trace', 'trace.erb')
+  def self.notice counter
+
+    puts if counter.joins.zero?
+
+    print ERB.new( File.read(
+      File.join('app', 'views', 'trace', 'trace.erb')
     )).result(
       OpenStruct.new( counter.instance_values )
-      .instance_eval { binding }
-    ).delete! "\n"
+      .instance_eval { binding } ).delete! "\n"
+
   end
 
 end
