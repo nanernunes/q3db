8f860b6463431be686545074ef28fb8ddfac1409
Naner Nunes <naner@naner.com.br>
2014-12-25 15:53:27 -0200

    Replaced strings inside QuakeTrace to ERB template

--- lib/quaketrace.rb
+++ lib/quaketrace.rb
@@ -1,16 +1,17 @@
+require 'erb'
+require 'ostruct'
 require 'quakecolor'
 
 module QuakeTrace
 
-  def self.notice counter
+  def self.notice(counter)
     print "\n" if counter.joins.zero?
-    print "\r"
-    print "#{ I18n.t(:match) }: [ ".bold
-    print "#{ I18n.t(:joins) }: #{ counter.joins.to_s.rjust(2, '0').green  } - "
-    print "#{ I18n.t(:items) }: #{ counter.items.to_s.rjust(4, '0').blue   } - "
-    print "#{ I18n.t(:chats) }: #{ counter.chats.to_s.rjust(3, '0').yellow } - "
-    print "#{ I18n.t(:kills) }: #{ counter.kills.to_s.rjust(3, '0').red    } "
-    print "]".bold
+    print ERB.new( File.read(File.join(
+      'app', 'views', 'trace', 'trace.erb')
+    )).result(
+      OpenStruct.new( counter.instance_values )
+      .instance_eval { binding }
+    ).delete! "\n"
   end
 
 end
