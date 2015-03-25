36f6c8529007390a0b4cac0343b2c5ab76543690
Naner Nunes <naner@naner.com.br>
2015-03-24 22:52:21 -0300

    Fix verbose when resuming a partial match from log

--- lib/quakecount.rb
+++ lib/quakecount.rb
@@ -3,10 +3,11 @@ class QuakeCount
 
   attr_reader :joins, :items, :chats, :kills
 
-  def startup
+  def initialize
     @joins = @items = @chats = @kills = 0
   end
 
+  def startup; initialize ; end
   def join_up; @joins += 1; end
   def item_up; @items += 1; end
   def chat_up; @chats += 1; end
